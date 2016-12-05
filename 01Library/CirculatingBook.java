bbd1ef4e2e4b7c5e70f4bd25d5aa4de7e2e2b864
raythejones <rayjones1111@gmail.com>
2016-12-05 01:45:55 -0500

    minor tweaks

--- 01Library/CirculatingBook.java
+++ 01Library/CirculatingBook.java
@@ -1,51 +1,51 @@
-public class CirculatingBook extends LibraryBook {
+public class CirculatingBook extends LibraryBook{
     private String currentHolder;
     private String dueDate;
 
-    public CirculatingBook(String author, String title, String ISBN, String callNumber) {
+    public CirculatingBook(String author, String title, String ISBN, String callNumber){
 	super(author, title, ISBN, callNumber);
     }
 
-    public String currentHolder() {
+    public String currentHolder(){
 	return currentHolder;
     }
 
-    public String dueDate() {
+    public String dueDate(){
 	return dueDate;
     }
 
-    public void setCurrentHolder(String newHolder) {
+    public void setCurrentHolder(String newHolder){
 	currentHolder = newHolder;
     }
 
-    public void setDueDate(String newDate) {
+    public void setDueDate(String newDate){
 	dueDate = newDate;
     }
 
-    public void checkout(String patron, String due) {
+    public void checkout(String patron, String due){
 	setCurrentHolder(patron);
 	setDueDate(due);
     }
-
-    public void returned() {
+    
+    public void returned(){
 	currentHolder = null;
 	dueDate = null;
     }
 
-    public String circulationStatus() {
-	if (currentHolder == null && dueDate == null) {
+    public String circulationStatus(){
+	if (currentHolder == null && dueDate == null){
 	    return "book available on shelves";
 	}
-	else {
+	else{
 	    return currentHolder + ", " + dueDate;
 	}
     }
 
-    public String toString() {
-	if (currentHolder == null && dueDate == null) {
+    public String toString(){
+	if (currentHolder == null && dueDate == null){
 	    return super.toString();
 	}
-	else {
+	else{
 	    return super.toString() + ", " + currentHolder + ", " + dueDate;
 	}
     }
--- 01Library/ReferenceBook.java
+++ 01Library/ReferenceBook.java
@@ -1,32 +1,32 @@
-public class ReferenceBook extends LibraryBook {
+public class ReferenceBook extends LibraryBook{
     private String collection;
 
-    public ReferenceBook(String author, String title, String ISBN, String callNumber, String collection) {
+    public ReferenceBook(String author, String title, String ISBN, String callNumber, String collection){
 	super(author, title, ISBN, callNumber);
 	this.collection = collection;
     }
     
-    public String collection() {
+    public String collection(){
 	return collection;
     }
 
-    public void setCollection(String collection) {
+    public void setCollection(String collection){
 	this.collection = collection;
     }
 
-    public void checkout(String patron, String due) {
+    public void checkout(String patron, String due){
 	System.out.println("cannot check out a reference book");
     }
 
-    public void returned() {
+    public void returned(){
 	System.out.println("reference book could not have been checked out -- return impossible");
     }
 
-    public String circulationStatus() {
+    public String circulationStatus(){
 	return "non-circulating reference book";
     }
 
-    public String toString() {
+    public String toString(){
 	return super.toString() + ", " + collection;
     }
 
