aee1ba6541a613a35445444efe6cb7b31a5ce38e
raythejones <rayjones1111@gmail.com>
2016-12-11 11:42:16 -0500

    the clone method and Constructor now work in a cooler way

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -16,39 +16,43 @@ public class Barcode implements Comparable<Barcode>{
   public Barcode(String zip) {
       	_zip = zip;
 	_checkDigit = 0;
-	 String[] codeKey = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::",};
-	//	    if (_zip.length() != 5){
-	//	    throw new IllegalArgumentException ("The input zip is not the right length.");
-	//	}
+       	    if (_zip.length() != 5){
+		    throw new IllegalArgumentException ("The input zip is not the right length.");
+		}
+	    try {
+	   Integer.parseInt(zip);
+       }
+	    catch (NumberFormatException e) {
+	    throw new IllegalArgumentException("One or more characters in the zipcode is not a digit.");
+       }
+	    
 	String codedZip = "";
-
-
-	//CHECKDIGIT
 	
-	for(int x=0;x<5;x++){
-	      char current = _zip.charAt(x); 
-	      int toBeAdded = current - '0';
-	      _checkDigit += toBeAdded;
-
-	}
-      _checkDigit = _checkDigit % 10;
+	//CHECKDIGIT
+	_checkDigit = checkSum() % 10;
  
 
-      /*
-      for(int x=0;x<5;x++){
-	  char q = zip.charAt(x);
-	  String r = str(q);
-	  codedZip += codeKey[r.parseInt];
-      }
 
-      _zip = codedZip;*/
+
+	
+	
   }
- /*
+    
+ 
 // postcondition: Creates a copy of a bar code.
   public Barcode clone(){
-      String codedCopy = _zip;	  }
+     
+      String ans = _zip;
 
- */
+   
+	      Barcode cloned = new Barcode(ans);
+	      return cloned;
+
+
+	      
+  }
+
+ 
       public String zip() {
       return _zip;
   }
@@ -57,27 +61,37 @@ public class Barcode implements Comparable<Barcode>{
   }
 // postcondition: computes and returns the check sum for _zip
   private  int checkSum(){
-      int output = Integer.parseInt(_zip);
-      int sum = 0;
-      int i = 1;
-      while (i < 5) {
-	  sum += (output % 10 ^ i);
-	  i++;
-      }
-      return sum % 10;
+            int ans = 0;
+	    for(int i=0;i<5;i++){
+	  int output = Integer.parseInt(_zip.substring(i,i+1));
+	  ans += output;
+  }
+      return ans;
 
   }
    
-    /*
+    
 //postcondition: format zip + check digit + Barcode 
 //ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
   public String toString(){
-	return "|" + _zip + _checkDigit + "|";
+      String tempZip = _zip;
+      String codedZip = "";
+      	 String[] codeKey = {"||:::", ":::||", "::|:|", "::||:", ":|::|", ":|:|:", ":||::", "|:::|", "|::|:", "|:|::",};
+
+
+	 for(int i=0;i<5;i++){
+	  int output = Integer.parseInt(tempZip.substring(i,i+1));
+	  codedZip += codeKey[output];
+	} 
+	codedZip += codeKey[_checkDigit];
+
+	
+      return "|" + codedZip  + "|";
 
 
 
   }    
-
+/*
 
 // postcondition: compares the zip + checkdigit, in numerical order. 
   public int compareTo(Barcode other){
@@ -87,12 +101,7 @@ public class Barcode implements Comparable<Barcode>{
   }
     */
 
-        public static void main(String[]args){
-	Barcode a = new Barcode("08451");
-	System.out.println(a.toString());
-	Barcode b = new Barcode ("12345");
-	System.out.println(b.toString());
-	}
+      
 
 public int compareTo(Barcode other){
       int z = Integer.parseInt(_zip + _checkDigit);
@@ -107,4 +116,15 @@ public int compareTo(Barcode other){
 	  return 0;
       }
 }
+
+    
+      public static void main(String[]args){
+	Barcode a = new Barcode("08451");
+	System.out.println(a.toString());
+
+	Barcode b = new Barcode ("12345");
+	System.out.println(b.toString());
+	System.out.println(b.clone());
+
+      }
 }
