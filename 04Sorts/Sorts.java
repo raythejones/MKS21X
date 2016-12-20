f83909d59153e86637551c70118a2270c8a7acdb
raythejones <rayjones1111@gmail.com>
2016-12-20 07:25:54 -0500

    realized i still had an issue in bubbleSort, but i fixed it

--- 04Sorts/Sorts.java
+++ 04Sorts/Sorts.java
@@ -31,28 +31,40 @@ public class Sorts{
 
         public static void insertionSort(int[] data){
 	int temp;
-	for(int x=0; x<data.length - x1; x++){
+	for(int x=0; x<data.length - x; x++){
 	    if(!(data[x] <= data[x + 1])){
 		temp = data[x + 1];
-		for(int y = 0; y <= i; y++){
+		for(int y = 0; y <= x; y++){
 		    if(temp <= data[y]){
 			for(int z = x + 1; z > y; z = z - 1){
 			    data[z] = data[z - 1];
 			}
 			data[y] = temp;
-			break;
+			return;
 		    }
 		}
 	    }
 	}
 	}
 
+    public static void swap(int[] data, int x, int y){
+      int t = data[x];
+      data[x] = data[y];
+      data[y] = t;
+    }
+    
     public static void bubbleSort(int[] data){
-	int len = data.length;
-	for(int n=len-1;n>1;n--){
-	    for(int x=0;x<n+1;x++){
+			int place = data.length;
+
+			for(int x1=0;x1<data.length;x1++){
+	    
+	    for(int x2=0;x2<place - 1;x2++){
+		if(data[x2] > data[x2+1]){
+		    swap(data, x2, x2+1);
+		}
 		
 	    }
+	    place--;
 	}
 	    
     }
@@ -65,10 +77,13 @@ public class Sorts{
 	    output += ",";
 	}
 	output += "]";
+	System.out.println(output);
 	}
     
     
     public static void main(String [] args) {
+
+
 }
 
 }
